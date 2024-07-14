7867a951af7cfb9c41a4fbd62adeeb9b7b524b92
Bashir Rayaneh <zakiyeh.hosseinzadeh@gmail,com>
2024-07-14 09:29:39 +0330

    [fix]:first commit

---
+++ scripts/theScripts.js
@@ -0,0 +1,12 @@
+$('.bxslider').bxSlider({
+    mode: 'fade',
+    speed: 700,
+    startSlide: 1,
+    control: false,
+    //       // nextSelector: $('#nextSlide'),
+    //       //   prevSelector : $('#prevSlide'),
+    //       //   nextText : '<i class="fa-solid fa-angle-right"></i>',
+    //       //   prevText : '<i class="fa-solid fa-angle-left"></i>',
+    auto: false,
+
+})
